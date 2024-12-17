83cf172f4046902f4465508537ace7d5675e5d89
CoderPEPE <esoph.dev.9@gmail.com>
2024-12-17 06:20:27 -0500

    feat: add orca_swap state

--- programs/solana-dex-aggregator/src/state/swap.rs
+++ programs/solana-dex-aggregator/src/state/swap.rs
@@ -1,9 +1,13 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
+use anchor_spl::{
+    token::{self, Token, TokenAccount},
+    token_interface::{Mint, TokenAccount as InterfaceTokenAccount, TokenInterface},
+};
 use raydium_cpmm_cpi::{
     program::RaydiumCpmm,
     states::{AmmConfig, ObservationState, PoolState},
 };
+use whirlpool_cpi::{self, program::Whirlpool as WhirlpoolProgram, state::*};
 
 #[derive(Accounts)]
 pub struct RaydiumSwap<'info> {
@@ -31,25 +35,25 @@ pub struct RaydiumSwap<'info> {
 
     /// The user token account for input token
     #[account(mut)]
-    pub input_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
+    pub input_token_account: Box<InterfaceAccount<'info, InterfaceTokenAccount>>,
 
     /// The user token account for output token
     #[account(mut)]
-    pub output_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
+    pub output_token_account: Box<InterfaceAccount<'info, InterfaceTokenAccount>>,
 
     /// The vault token account for input token
     #[account(
       mut,
       constraint = input_vault.key() == pool_state.load()?.token_0_vault || input_vault.key() == pool_state.load()?.token_1_vault
   )]
-    pub input_vault: Box<InterfaceAccount<'info, TokenAccount>>,
+    pub input_vault: Box<InterfaceAccount<'info, InterfaceTokenAccount>>,
 
     /// The vault token account for output token
     #[account(
       mut,
       constraint = output_vault.key() == pool_state.load()?.token_0_vault || output_vault.key() == pool_state.load()?.token_1_vault
   )]
-    pub output_vault: Box<InterfaceAccount<'info, TokenAccount>>,
+    pub output_vault: Box<InterfaceAccount<'info, InterfaceTokenAccount>>,
 
     /// SPL program for input token transfers
     pub input_token_program: Interface<'info, TokenInterface>,
@@ -71,4 +75,43 @@ pub struct RaydiumSwap<'info> {
     /// The program account for the most recent oracle observation
     #[account(mut, address = pool_state.load()?.observation_key)]
     pub observation_state: AccountLoader<'info, ObservationState>,
+}
+
+#[derive(Accounts)]
+pub struct OrcaSwap<'info> {
+    pub whirlpool_program: Program<'info, WhirlpoolProgram>,
+
+    #[account(address = token::ID)]
+    pub token_program: Program<'info, Token>,
+
+    pub token_authority: Signer<'info>,
+
+    #[account(mut)]
+    pub whirlpool: Box<Account<'info, Whirlpool>>,
+
+    #[account(mut, constraint = token_owner_account_a.mint == whirlpool.token_mint_a)]
+    pub token_owner_account_a: Box<Account<'info, TokenAccount>>,
+    #[account(mut, address = whirlpool.token_vault_a)]
+    pub token_vault_a: Box<Account<'info, TokenAccount>>,
+
+    #[account(mut, constraint = token_owner_account_b.mint == whirlpool.token_mint_b)]
+    pub token_owner_account_b: Box<Account<'info, TokenAccount>>,
+    #[account(mut, address = whirlpool.token_vault_b)]
+    pub token_vault_b: Box<Account<'info, TokenAccount>>,
+
+    #[account(mut)]
+    /// CHECK: checked by whirlpool_program
+    pub tick_array_0: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    /// CHECK: checked by whirlpool_program
+    pub tick_array_1: UncheckedAccount<'info>,
+
+    #[account(mut)]
+    /// CHECK: checked by whirlpool_program
+    pub tick_array_2: UncheckedAccount<'info>,
+
+    #[account(mut, seeds = [b"oracle", whirlpool.key().as_ref()], bump, seeds::program = whirlpool_program.key())]
+    /// CHECK: checked by whirlpool_program
+    pub oracle: UncheckedAccount<'info>,
 }
